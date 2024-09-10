1989c225f206734dca1d452fd5f1ecc1112dad11
haydencfb <haydenfitzbrintle@gmail.com>
2024-09-10 12:21:08 -0700

    Learning how to work with multiple JSX files

--- personal-portfolio-client/src/main.jsx
+++ personal-portfolio-client/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './styles/index.css'
+import './styles/main.css'
+import Header from './Header.jsx'
+import Home from './Home.jsx'
+import About from './About.jsx'
+import Footer from './Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Header />
+    <Home /> 
+    <About /> 
+    <Footer />
   </StrictMode>,
 )
